9dc964916b25b98c6e2980600c04563f11318abb
[Elif Gümüş] <[elif.gumus@bil.omu.edu.tr]>
2023-10-22 03:44:44 +0300

    The Sign Up page has been changed to use Formik and Yup.
    The TextInput and CheckBox Components were created using Formik and Yup, These components are designed to be used in Forms.

---
+++ src/components/Form/Checkbox.js
@@ -0,0 +1,23 @@
+import { useField } from 'formik';
+import Styles from "./Styles.module.css";
+
+const Checkbox = ({ children, ...props }) => {
+    // React treats radios and checkbox inputs differently from other input types: select and textarea.
+    // Formik does this too! When you specify `type` to useField(), it will
+    // return the correct bag of props for you -- a `checked` prop will be included
+    // in `field` alongside `name`, `value`, `onChange`, and `onBlur`
+    const [field, meta] = useField({ ...props, type: 'checkbox' });
+    return (
+      <div>
+        <label className="checkbox-input">
+          <input type="checkbox" {...field} {...props} />
+          {children}
+        </label>
+        {meta.touched && meta.error ? (
+          <div className={Styles.error}>{meta.error}</div>) : null
+        }
+      </div>
+    );
+};
+
+  export default Checkbox;
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,121 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Formik, Form, useField } from 'formik';
+import * as Yup from 'yup';
+
+import Styles from "./Styles.module.css";
+import MyTextInput from '../../components/Form/TextInput';
+import MyCheckbox from '../../components/Form/Checkbox';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import { Container, CssBaseline, Typography } from '@mui/material';
+
+import { useNavigate } from "react-router-dom";
+
+
+
+const defaultTheme = createTheme();
+
+
+
 
 function SignUp() {
+    let navigate = useNavigate(); 
+
+    const routeChange = (isLoggedIn) =>{ 
+        let path = `/signin`; 
+        navigate(path);
+    }
     return (
-      <div>SignUp</div>
-    )
-  }
+        <ThemeProvider theme={defaultTheme}>
+            <Container maxWidth="xs">
+                <CssBaseline>
+                    <Box 
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            marginTop: 1,
+                            marginBottom: 18,
+                        }}
+                    >
+                        <Typography variant="h5" sx={{ mb: 2 }}>
+                            Sign Up
+                        </Typography>
+
+                        <Formik
+                            initialValues={{
+                                firstName: '',
+                                lastName: '',
+                                email: '',
+                                password: '',
+                                acceptedTerms: false, // added for our checkbox
+                            }}
+                            validationSchema={Yup.object({
+                                firstName: Yup.string()
+                                    .max(15, 'Must be 15 characters or less')
+                                    .required('First name is Required'),
+                                lastName: Yup.string()
+                                    .max(20, 'Must be 20 characters or less')
+                                    .required('Last name is Required'),
+                                email: Yup.string()
+                                    .email('Invalid email address')
+                                    .required('Email is Required'),
+                                password: Yup.string()
+                                    .matches(
+                                        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/, 
+                                        'Must Contain at least8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character')
+                                    .required('Password is Required'),
+                                acceptedTerms: Yup.boolean()
+                                    .required('Required')
+                                    .oneOf([true], 'You must accept the terms and conditions.'),
+                            })}
+                            onSubmit={(values, { setSubmitting }) => {
+                                setTimeout(() => {
+                                    routeChange();
+                                    alert(JSON.stringify(values, null, 2));
+                                    setSubmitting(false);
+                                }, 400);
+                            }}
+                        >
+                            <Form className={Styles.form}>
+                                <MyTextInput
+                                    label="First Name"
+                                    name="firstName"
+                                    type="text"
+                                />
+                        
+                                <MyTextInput
+                                    label="Last Name"
+                                    name="lastName"
+                                    type="text"
+                                />
+                        
+                                <MyTextInput
+                                    label="Email Address"
+                                    name="email"
+                                    type="email"
+                                />
+
+                                <MyTextInput 
+                                    label="Password"
+                                    name="password"
+                                    type="password"
+                                />
+                        
+                                <MyCheckbox name="acceptedTerms">
+                                    I accept the terms and conditions
+                                </MyCheckbox>
+                        
+                                <button type="submit">Submit</button>
+                            </Form>
+                        </Formik>
+                    </Box>
+                </CssBaseline>
+            </Container>
+        </ThemeProvider>
+    );
+}
   
   export default SignUp
