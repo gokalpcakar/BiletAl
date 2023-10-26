508bcab5c04fd3101659d35f2fc9f148223c80ec
fatmadurak <fatmabadillidurak@gmail.com>
2023-10-26 14:20:02 +0300

    fetching event details using slug from the API

--- src/pages/EventDetail/index.js
+++ src/pages/EventDetail/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { getByEventsById } from "../../network/requests/EventServices";
+import { getAllSlug } from "../../network/requests/EventServices";
 import { useQuery } from "react-query";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
@@ -48,9 +48,9 @@ function EventDetail() {
     setValue(newValue);
   };
 
-  const { id } = useParams();
-  const { isLoading, error, data } = useQuery(["events", id], () =>
-    getByEventsById(id)
+  const { slug } = useParams();
+  const { isLoading, error, data } = useQuery(["events", slug], () =>
+    getAllSlug(slug)
   );
 
   if (isLoading) {
@@ -64,7 +64,7 @@ function EventDetail() {
   return (
     <div>
       <Carousel className={styles.homePageCarousel} showArrows={true} showStatus={false} showThumbs={false}>
-        {data.images.map((image, index) => (
+        {data[0]?.images.map((image, index) => (
           <div key={index}>
             <img src={image} alt={`Image ${index}`} />
           </div>
@@ -82,26 +82,26 @@ function EventDetail() {
                 </Tabs>
               </Box>
               <CustomTabPanel value={value} index={0}>
-                {data.name}<br />
-                {data.description}<br />
+                {data[0].name}<br />
+                {data[0].description}<br />
               </CustomTabPanel>
               <CustomTabPanel value={value} index={1}>
-                {data.name}<br />
-                Kategori: {data.eventType}<br />
-                Şehir: {data.city}<br />
-                Başlangıç tarihi: {data.startDate}<br />
-                Bitiş tarihi: {data.endDate}<br />
-                Ücret: {data.price}
+                {data[0].name}<br />
+                Kategori: {data[0].eventType}<br />
+                Şehir: {data[0].city}<br />
+                Başlangıç tarihi: {data[0].startDate}<br />
+                Bitiş tarihi: {data[0].endDate}<br />
+                Ücret: {data[0].price}
               </CustomTabPanel>
             </Box>
           </Grid>
           <Grid item={true} lg={6} xs={12} mt={{ lg: 0, xs: 2 }} pl={{ lg: 2, xs: 0 }}>
             <div className={styles.map}>
               <iframe
-                src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d304282.5343310836!2d32.52732558689937!3d39.9344112443957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cfbb769c32db85%3A0x2d276a630ac725b8!2s${data.city}!5e0!3m2!1str!2str!4v1698163533517`}
+                src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d304282.5343310836!2d32.52732558689937!3d39.9344112443957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cfbb769c32db85%3A0x2d276a630ac725b8!2s${data[0].city}!5e0!3m2!1str!2str!4v1698163533517`}
                 width="100%"
                 height="400"
-                title={data.city}
+                title={data[0].city}
                 loading="lazy"
                 style={{ borderRadius: "8px" }}
               ></iframe>
