0172b23f0a866539666dbbeadc4e82df401e7fdd
[Elif Gümüş] <[elif.gumus@bil.omu.edu.tr]>
2023-10-28 20:45:26 +0300

    Scroll To Top Button has been added to main layout.

---
+++ src/components/ScrollToTop/index.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react'; 
+import ArrowCircleUpSharpIcon from '@mui/icons-material/ArrowCircleUpSharp';
+import Box from '@mui/material/Box';
+
+function ScrollToTop() {
+  const [visible, setVisible] = useState(false) 
+  
+  const toggleVisible = () => { 
+    const scrolled = document.documentElement.scrollTop; 
+    if (scrolled > 300){ 
+      setVisible(true) 
+    }  
+    else if (scrolled <= 300){ 
+      setVisible(false) 
+    } 
+  }; 
+  
+  const scrollToTop = () =>{ 
+    window.scrollTo({ 
+      top: 0,  
+      behavior: 'smooth'
+    }); 
+  }; 
+  
+  window.addEventListener('scroll', toggleVisible); 
+
+  return ( 
+    <Box sx={{
+      position: "fixed",
+      bottom: "20px",
+      right: "20px",
+      zIndex: "1",
+      cursor: "pointer",
+      color: "green",
+    }}> 
+      <ArrowCircleUpSharpIcon 
+        onClick={scrollToTop}  
+        style={{display: visible ? 'inline' : 'none'}} 
+        sx={{ fontSize: 50}}
+      /> 
+    </Box> 
+  ); 
+}
+
+export default ScrollToTop;
--- src/layout/main/index.js
+++ src/layout/main/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import mainLayout from "./mainLayout.module.css";
+import ScrollToTop from "../../components/ScrollToTop";
 
 function MainLayout({ children }) {
   return (
@@ -10,6 +11,7 @@ function MainLayout({ children }) {
         <Navbar />
         <div className={mainLayout.content}>{children}</div>
         <Footer />
+        <ScrollToTop />
       </div>
     </>
   );
