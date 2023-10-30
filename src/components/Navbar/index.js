65e3b7098d0a0f8fa2306a2c5acec4927cdab953
fatmadurak <fatmabadillidurak@gmail.com>
2023-10-30 13:16:40 +0300

    changes filter,updated date filter

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -24,7 +24,7 @@ const Navbar = () => {
   const handleClick = () => {
     setSearchResults(data);
   };
-  const handleClickConcert=()=>{
+  const handleSearchClick=()=>{
 
     setSearchResults(searchData)
 
@@ -70,10 +70,10 @@ const Navbar = () => {
                 onChange={(e, value) => setValue(value)}
               >
                 <Tab label="Anasayfa" component={Link} to="/" value="/" onClick={handleClick} />
-                <Tab label="Konser" component={Link} to="/concert" value="/concert" onClick={handleClickConcert} />
-                <Tab label="Festival" component={Link} to="/festival" value="/festival" />
-                <Tab label="Stand-Up" component={Link} to="/standup" value="/standup" />
-                <Tab label="Tiyatro" component={Link} to="/theatre" value="/theatre" />
+                <Tab label="Konser" component={Link} to="/concert" value="/concert" onClick={handleSearchClick} />
+                <Tab label="Festival" component={Link} to="/festival" value="/festival" onClick={handleSearchClick}  />
+                <Tab label="Stand-Up" component={Link} to="/standup" value="/standup"  onClick={handleSearchClick} />
+                <Tab label="Tiyatro" component={Link} to="/theatre" value="/theatre"  onClick={handleSearchClick} />
                 <Tab label="Geçmiş Etkinlikler" component={Link} to="/pastevents" value="/pastevents" />
               </Tabs>
               <SearchBar />
