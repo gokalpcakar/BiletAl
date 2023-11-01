1cf6337383a9d9405021d4939f636182c94c944b
fatmadurak <fatmabadillidurak@gmail.com>
2023-11-01 14:03:37 +0300

    added sugify  ,edit detail page,delete slug data

--- src/pages/EventDetail/index.js
+++ src/pages/EventDetail/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { getAllSlug } from "../../network/requests/EventServices";
+import { getEvents } from "../../network/requests/EventServices";
 import { useQuery } from "react-query";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import styles from "./style.module.css";
@@ -12,15 +12,14 @@ import Box from '@mui/material/Box';
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import ImageGallery from "react-image-gallery";
-import ShareButtons from "../../components/ShareButtons"
+import ShareButtons from "../../components/ShareButtons";
 
 function CustomTabPanel(props) {
-
   const { children, value, index, ...other } = props;
 
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <div
@@ -47,17 +46,27 @@ function a11yProps(index) {
 }
 
 function EventDetail() {
-
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  const { slug } = useParams();
-  const { isLoading, error, data } = useQuery(["events", slug], () =>
-    getAllSlug(slug)
-  );
+  function slugify(text) {
+    if (text && text.toString) {
+      return text.toString().toLowerCase()
+        .replace(/\s+/g, '/') // Boşlukları tire ile değiştir
+        .replace(/[^\w-]+/g, '') // Alfanümerik olmayan karakterleri kaldır
+        .replace(/--+/g, '-') // Ardışık tireleri birleştir
+        .replace(/^-+/, '') // Başındaki tıreleri kaldır
+        .replace(/-+$/, ''); // Sonundaki tıreleri kaldır
+    } else {
+      return '';
+    }
+  }
+
+  const { name = ' ' } = useParams();
+  const { isLoading, error, data } = useQuery("events", getEvents);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -67,18 +76,25 @@ function EventDetail() {
     return <div>Error: {error.message}</div>;
   }
 
-  const images = data[0]?.images.map((image, index) => ({
-    original: image,
+  const matchedEvent = data.find((item) =>
+    slugify(item.name).includes(slugify(name))
+  );
 
+  if (!matchedEvent) {
+    return <div>Event not found</div>;
+  }
+
+  const images = matchedEvent.images.map((image, index) => ({
+    original: image,
   }));
+
   return (
     <div>
-
       <div className={styles.gallery}>
         <ImageGallery items={images} showPlayButton={false} />
       </div>
 
-      <Container maxWidth="xl" >
+      <Container maxWidth="xl">
         <Grid container spacing={1} sx={{ mb: "3rem", display: 'flex', mt: "3rem" }}>
           <Grid item={true} lg={4} xs={12}>
             <Box className={styles.tabPanelBox}>
@@ -89,26 +105,26 @@ function EventDetail() {
                 </Tabs>
               </Box>
               <CustomTabPanel value={value} index={0}>
-                {data[0].name}<br />
-                {data[0].description}<br />
+                {matchedEvent.name}<br />
+                {matchedEvent.description}<br />
               </CustomTabPanel>
               <CustomTabPanel value={value} index={1}>
-                {data[0].name}<br />
-                Kategori: {data[0].eventType}<br />
-                Şehir: {data[0].city}<br />
-                Başlangıç tarihi: {data[0].startDate}<br />
-                Bitiş tarihi: {data[0].endDate}<br />
-                Ücret: {data[0].price}
+                {matchedEvent.name}<br />
+                Kategori: {matchedEvent.eventType}<br />
+                Şehir: {matchedEvent.city}<br />
+                Başlangıç tarihi: {matchedEvent.startDate}<br />
+                Bitiş tarihi: {matchedEvent.endDate}<br />
+                Ücret: {matchedEvent.price}
               </CustomTabPanel>
             </Box>
           </Grid>
           <Grid item={true} lg={8} xs={12} mt={{ lg: 0, xs: 2 }} pl={{ lg: 2, xs: 0 }}>
             <div className={styles.map}>
               <iframe
-                src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d304282.5343310836!2d32.52732558689937!3d39.9344112443957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cfbb769c32db85%3A0x2d276a630ac725b8!2s${data[0].city}!5e0!3m2!1str!2str!4v1698163533517`}
+                src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d304282.5343310836!2d32.52732558689937!3d39.9344112443957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cfbb769c32db85%3A0x2d276a630ac725b8!2s${matchedEvent.city}!5e0!3m2!1str!2str!4v1698163533517`}
                 width="100%"
                 height="400"
-                title={data[0].city}
+                title={matchedEvent.city}
                 loading="lazy"
                 style={{ borderRadius: "8px" }}
               ></iframe>
@@ -117,11 +133,11 @@ function EventDetail() {
         </Grid>
       </Container>
       <ShareButtons
-        eventName={data[0].name}
-        eventType={data[0].eventType}
-        eventDescription={data[0].description}
+        eventName={matchedEvent.name}
+        eventType={matchedEvent.eventType}
+        eventDescription={matchedEvent.description}
       />
-    </div >
+    </div>
   );
 }
 
