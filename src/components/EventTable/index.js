1ff953554193fbc586c1c82f725e95e174451eea
[Elif Gümüş] <[elif.gumus@bil.omu.edu.tr]>
2023-11-01 22:36:41 +0300

    Added Pagination to Admin Panel Table

---
+++ src/components/EventTable/index.js
@@ -0,0 +1,130 @@
+import React from 'react'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+import { useQuery } from "react-query";
+import { getEvents } from "../../network/requests/EventServices";
+import { Container, Grid } from "@mui/material";
+import ClearIcon from '@mui/icons-material/Clear';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import TablePaginationActions from "./TablePaginationActions";
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(even)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.info.main,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+function EventTable() {
+  const { data, isLoading } = useQuery("events", getEvents);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - data?.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+  
+  return (
+    <>
+      {isLoading ? (<div>Loading...</div>) : data? ( 
+        <Container maxWidth="lg" sx={{marginTop:"7%", marginBottom:"4%"}}>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 900 }}>
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell>ID</StyledTableCell>
+                  <StyledTableCell align="left">İsim</StyledTableCell>
+                  <StyledTableCell align="left">Şehir</StyledTableCell>
+                  <StyledTableCell align="left">Mekan</StyledTableCell>
+                  <StyledTableCell align="left">Etkinlik Türü</StyledTableCell>
+                  <StyledTableCell align="left">Başlangıç Tarihi</StyledTableCell>
+                  <StyledTableCell align="left">Bitiş Tarihi</StyledTableCell>
+                  <StyledTableCell align="left">Ücret&nbsp;(TL)</StyledTableCell>
+                  <StyledTableCell align="left"></StyledTableCell>
+                  <StyledTableCell align="left"></StyledTableCell>
+                  <StyledTableCell align="left"></StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {(rowsPerPage > 0
+                  ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  : data
+                ).map((row) => (
+                  <StyledTableRow key={row.id}>
+                    <StyledTableCell component="th" scope="row"><strong>{row.id}</strong></StyledTableCell>
+                    <StyledTableCell align="left">{row.name}</StyledTableCell>
+                    <StyledTableCell align="left">{row.city}</StyledTableCell>
+                    <StyledTableCell align="left">{row.location}</StyledTableCell>
+                    <StyledTableCell align="left">{row.eventType}</StyledTableCell>
+                    <StyledTableCell align="left">{row.startDate}</StyledTableCell>
+                    <StyledTableCell align="left">{row.endDate}</StyledTableCell>
+                    <StyledTableCell align="left">{row.price}</StyledTableCell>
+                    <StyledTableCell align="left"><ClearIcon /></StyledTableCell>
+                    <StyledTableCell align="left"><EditIcon /></StyledTableCell>
+                    <StyledTableCell align="left"><AddIcon /></StyledTableCell>
+                  </StyledTableRow>
+                ))}
+                {emptyRows > 0 && (
+                  <TableRow style={{ height: 53 * emptyRows }}>
+                    <TableCell colSpan={11} />
+                  </TableRow>
+                )}
+              </TableBody>
+              <TableFooter>
+                <TableRow>
+                  <TablePagination
+                    rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                    colSpan={11}
+                    count={data.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    SelectProps={{
+                      inputProps: {
+                        'aria-label': 'rows per page',
+                      },
+                      native: true,
+                    }}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                    ActionsComponent={TablePaginationActions}
+                  />
+                </TableRow>
+              </TableFooter>
+            </Table>
+          </TableContainer>
+        </Container>
+      ): (<div>No data available.</div>)}
+    </>
+  )
+}
+
+export default EventTable;
