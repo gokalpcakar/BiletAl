7a6a2c3b6fcdb9b972db4bc068879bdd5f3e990a
fatmadurak <fatmabadillidurak@gmail.com>
2023-11-28 14:56:57 +0300

    added navbar button styles

--- src/components/Navbar/NavbarButtonGroup/index.js
+++ src/components/Navbar/NavbarButtonGroup/index.js
@@ -1,9 +1,11 @@
-import { Button } from "@mui/material";
+import { Button, useMediaQuery } from "@mui/material";
 import { Link } from "react-router-dom";
 
 function NavbarButtonGroup() {
+    const isSmallScreen = useMediaQuery('(max-width:1250px)');
+
     return (
-        <div>
+        <div style={{display:"flex", flexWrap:"nowrap"}}>
             <Button sx={{
                 marginLeft: "10px",
                 backgroundColor: "#7BD8C0",
@@ -13,7 +15,13 @@ function NavbarButtonGroup() {
                     color: '#7BD8C0',
                     transition: '.5s',
                     backgroundColor: '#034f84'
-                }
+                },
+                ...(isSmallScreen && {
+                    fontSize: '10px', 
+                    padding: '10px 5px', 
+                    marginLeft:"3px",
+                    fontWeight:"500",
+                })
             }} component={Link} to="/signin" variant="contained">
                 Giriş Yap
             </Button>
@@ -26,7 +34,13 @@ function NavbarButtonGroup() {
                     border: '1px solid #7BD8C0',
                     color: '#7BD8C0',
                     backgroundColor: '#034f84'
-                }
+                },
+                ...(isSmallScreen && {
+                    fontSize: '10px', 
+                    padding: '10px 5px',  
+                    marginLeft:"3px",
+                    fontWeight:"500",
+                })
             }} component={Link} to="/signup" variant="contained">
                 Kaydol
             </Button>
@@ -34,4 +48,4 @@ function NavbarButtonGroup() {
     )
 }
 
-export default NavbarButtonGroup
+export default NavbarButtonGroup;
